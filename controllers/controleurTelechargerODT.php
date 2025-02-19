a55bd86bcfcd50199fbc98fa8389a2340e5cfd32
SonicShulk100 <romain.mourouvin2004@gmail.com>
2025-02-19 17:57:35 +0100

    Correction du téléchargement ODT.

--- controllers/controleurTelechargerODT.php
+++ controllers/controleurTelechargerODT.php
@@ -2,8 +2,8 @@
 require_once "../models/DAO/FichierDAO.php";
 require_once "../vendor/autoload.php"; // Assurez-vous d'utiliser Composer pour PHPWord
 
-use PhpOffice\PhpWord\PhpWord;
 use PhpOffice\PhpWord\IOFactory;
+use PhpOffice\PhpWord\PhpWord;
 
 if (isset($_GET['idFichier'])) {
     $idFichier = (int) $_GET['idFichier'];
@@ -15,15 +15,26 @@ if (isset($_GET['idFichier'])) {
         $phpWord = new PhpWord();
         $section = $phpWord->addSection();
 
-        // Ajout du contenu dans le document ODT
-        $section->addText(htmlspecialchars_decode($fichier->getContenu()));
+        // Ajout du contenu XML dans le document ODT en gérant les namespaces
+        $xmlContent = htmlspecialchars_decode($fichier->getContenu());
+        $xml = simplexml_load_string($xmlContent, 'SimpleXMLElement', LIBXML_NOCDATA, 'office', true);
+
+        if ($xml === false) {
+            die('Erreur de chargement du fichier XML');
+        }
+
+        $namespaces = $xml->getNamespaces(true);
+        $xml->registerXPathNamespace('office', $namespaces['office']);
+        $text = (string) $xml->xpath('//office:body/office:text')[0];
+
+        $section->addText($text);
 
         // Définition du nom du fichier ODT
         $fileName = "Fichier_" . $fichier->getId() . ".odt";
 
         // Création du document ODT
         header("Content-Description: File Transfer");
-        header("Content-Disposition: attachment; filename={$fileName}");
+        header("Content-Disposition: attachment; filename=$fileName");
         header("Content-Type: application/vnd.oasis.opendocument.text");
 
         // Enregistrement dans un flux de sortie
